d8ff8a3bb20f1aff2717aa8798d52f126848ff9c
unknown <michaelcmatteis@gmail.com>
2024-11-26 21:36:35 -0600

    finishing header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/quiz-logo.png"
+const Header = ()=>{
+    
+    return <header>
+        <img src= {logo}/>
+        <h1>React Quiz</h1>
+    </header>
+}
+
+export default Header
